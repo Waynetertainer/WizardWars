0ad7828200660a53cb7f76d1065e536b5a21b669
Unknown <joshua.tobias@games-academy.de>
2018-12-06 16:56:26 +0100

    pathfinding

--- Project/Assets/Scripts/Tile.cs
+++ Project/Assets/Scripts/Tile.cs
@@ -1,5 +1,6 @@
 ﻿
 using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -73,6 +74,20 @@ public class Tile : MonoBehaviour
         return dy + Mathf.Max(0, (dx - dy) / 2);
     }
 
+    public List<Tile> GetNeighbours()
+    {
+        List<Tile> neighbours = new List<Tile>();
+        for (int j = 0; j < 6; j++)
+        {
+            Tile tempTile = GridManager.pInstance.GetNeighbour(this, j);
+            if (tempTile != null)
+            {
+                neighbours.Add(tempTile);
+            }
+        }
+        return neighbours;
+    }
+
     private void OnMouseEnter()
     {
 
@@ -83,14 +98,29 @@ public class Tile : MonoBehaviour
             mLine.SetPosition(0, transform.position + new Vector3(0, 0.1f, 0));
             mLine.SetPosition(1, GameManager.pInstance.pActiveCharacter.pTile.transform.position + new Vector3(0, 0.1f, 0));
 
+            GridManager.pInstance.DrawPath(GridManager.pInstance.GetPathTo(GameManager.pInstance.pActiveCharacter.pTile, this));
         }
 
+    }
+
+    private void MoveUp()
+    {
+        transform.position += Vector3.up;
+    }
+
+    private void MoveDown()
+    {
+        transform.position -= Vector3.up;
     }
 
     private void OnMouseExit()
     {
         mMouseOver = false;
         mLine.gameObject.SetActive(false);
+        for (int i = 0; i < GridManager.pInstance.pPathPainter.transform.childCount; i++)
+        {
+            GridManager.pInstance.pPathPainter.transform.GetChild(i).gameObject.SetActive(false);
+        }
     }
 
     private void Update()
@@ -99,15 +129,17 @@ public class Tile : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(1))
             {
-                int distance = Tile.Distance(GameManager.pInstance.pActiveCharacter.pTile, this);
-                for (int k = 1; k <= distance; k++)
-                {
-                    float Xf = ((GameManager.pInstance.pActiveCharacter.pTile.pPosition.x + ((float)(this.pPosition.x - GameManager.pInstance.pActiveCharacter.pTile.pPosition.x) / distance) * k));
-                    float Yf = ((GameManager.pInstance.pActiveCharacter.pTile.pPosition.y + ((float)(this.pPosition.y - GameManager.pInstance.pActiveCharacter.pTile.pPosition.y) / distance) * k));
-                    int y = Mathf.RoundToInt(Yf);
-                    int x = ((int)(Xf)) + ((y + ((int)Xf) % 2) % 2);
-                    Debug.Log("Knoten " + k + " | FloatX = " + Xf + " FloatY = " + Yf + " | RoundedX= " + "NI" + " | IntX = " + x + " IntY = " + y);
-                }
+                //int distance = Tile.Distance(GameManager.pInstance.pActiveCharacter.pTile, this);
+                //for (int k = 1; k <= distance; k++)
+                //{
+                //    float Xf = ((GameManager.pInstance.pActiveCharacter.pTile.pPosition.x + ((float)(this.pPosition.x - GameManager.pInstance.pActiveCharacter.pTile.pPosition.x) / distance) * k));
+                //    float Yf = ((GameManager.pInstance.pActiveCharacter.pTile.pPosition.y + ((float)(this.pPosition.y - GameManager.pInstance.pActiveCharacter.pTile.pPosition.y) / distance) * k));
+                //    int y = Mathf.RoundToInt(Yf);
+                //    int x = ((int)(Xf)) + ((y + ((int)Xf) % 2) % 2);
+                //    Debug.Log("Knoten " + k + " | FloatX = " + Xf + " FloatY = " + Yf + " | RoundedX= " + "NI" + " | IntX = " + x + " IntY = " + y);
+                //}
+
+
             }
         }
     }
