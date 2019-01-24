75246cc7468d9405ad43332ed8e06513b967a371
Unknown <joshua.tobias@games-academy.de>
2019-01-24 12:59:37 +0100

    tooltip 1

---
+++ Project/Assets/Scripts/UI/ToolTip.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ToolTip : Window
+{
+    public Text pNameText;
+    public Text pRangeText;
+    public Text pDamageText;
+    public Text pCooldownText;
+    public Text pDescriptionText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
